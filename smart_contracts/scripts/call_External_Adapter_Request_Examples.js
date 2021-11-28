6fdbf12edfd4ac0950e23a4c1eefa4c6f28622ad
Morgan Kuphal <87319522+KuphJr@users.noreply.github.com>
2021-11-27 19:02:29 -0600

    Update call_External_Adapter_Request_Examples.js

--- smart_contracts/scripts/call_External_Adapter_Request_Examples.js
+++ smart_contracts/scripts/call_External_Adapter_Request_Examples.js
@@ -15,25 +15,34 @@ async function main() {
   // set up a listener to print events emitted by the smart contract
   let filter = { address: smartContract.address };
   let provider = new ethers.providers.JsonRpcProvider("https://speedy-nodes-nyc.moralis.io/c13b64136506585cb50319e7/polygon/mumbai");
+  // decrements how many events have been caught
+  let i = 8;
   const listener = new Promise((resolve, reject) => provider.on(filter, async (event) => {
     if (event.topics.includes('0xb5e6e01e79f91267dc17b4e6314d5d4d03593d2ceee0fbb452b750bd70ea5af9')) {
       console.log("CHAINLINK REQUEST EVENT EMITTED");
+      i--;
     }
     if (event.topics.includes('0x8c2992c05dc87c07311894d1f943111cba4f45bbe1fd66dce65cc5db2c834359')) {
       let int256_result = await smartContract.int256_result();
       console.log("CHAINLINK ORACLE RETURNED INT256:", int256_result.toNumber());
+      i--;
     }
     if (event.topics.includes('0x55cbd3f8c60adc9f7984732c240ea015acf17284b6d83d670ba4ee42b44783ee')) {
       let uint256_result = await smartContract.uint256_result();
       console.log("CHAINLINK ORCALE RETURNED UINT256:", uint256_result.toNumber());
+      i--;
     }
     if (event.topics.includes('0x622c782522db5098e0f79fccd5c2b5b18a9717cdb1eb12934e4b1beb3b255377')) {
       let bool_result = await smartContract.bool_result();
       console.log("CHAINLINK ORCALE RETURNED BOOL:", bool_result);
+      i--;
     }
     if (event.topics.includes('0xdbb7bea395204da325e836f2375f392081152b224349387309f865df9c19ce2c')) {
       let bytes32_result = await smartContract.bytes32_result();
       console.log("CHAINLINK ORCALE RETURNED BYTES32:", ethers.utils.parseBytes32String(bytes32_result));
+      i--;
+    }
+    if (i === 0) {
       process.exit(0);
     }
   }));
