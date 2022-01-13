169c02cec0504cc8438efbc35c3f1e482f48b766
Morgabotics <87319522+KuphJr@users.noreply.github.com>
2022-01-13 11:40:11 -0600

    Added ability to use tmp directory
    Added importing external and builtin Node.js modules.  Also, requests can use the tmp directory, which is cleared out between each request.

--- external-adapter/Sandbox.js
+++ external-adapter/Sandbox.js
@@ -1,17 +1,17 @@
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
 const { NodeVM, VMScript } = require('vm2')
 
 class Sandbox {
   static async evaluate (javascriptString, vars) {
-    // const vm = new NodeVM({
-    //   console: 'off',
-    //   sandbox: vars,
-    //   require: true
-    // })
+    this.clearTmpDirectory()
     const vm = new NodeVM({
       console: 'off',
       sandbox: vars,
       require: {
-        external: true
+        external: true,
+        builtin: ['*']
       }
     })
     let functionScript
@@ -27,7 +27,19 @@ class Sandbox {
     } catch (runScriptError) {
       throw Error('Error evaluating provided JavaScript: ' + runScriptError)
     }
+    this.clearTmpDirectory()
     return result
+  }
+
+  static clearTmpDirectory () {
+    const dirents = fs.readdirSync(os.tmpdir())
+    dirents.forEach(dirent => {
+      if (fs.lstatSync(path.join(os.tmpdir(), dirent).isDirectory())) {
+        fs.rmdirSync(path.join(os.tmpdir(), dirent), { recursive: true })
+      } else {
+        fs.rmSync(path.join(os.tmpdir(), dirent))
+      }
+    })
   }
 }
 
